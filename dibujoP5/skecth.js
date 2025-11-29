88bbf6939ffb9bdbc4af5e414f431d8d05d466e8
Eduardo2233073297 <166538616+Eduardo2233073297@users.noreply.github.com>
2025-11-28 22:22:26 -0600

    Add files via upload

---
+++ dibujoP5/skecth.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0,200,200);
+  fill("blue");
+  ellipse(55, 50, 80, 80);
+
+  fill("purple");
+    ellipse(80, 85, 80, 80);
+
+     fill("red");
+    ellipse(105, 120, 80, 80);
+
+    fill("orange");
+    ellipse(130, 151, 80, 80);
+
+    fill("yellow");
+    ellipse(155, 175, 80,);
+  
+  fill("green");
+  rect(0, 200, 600, 200);
+
+}
